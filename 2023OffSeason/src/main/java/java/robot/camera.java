321a8e8bfb777c08f6600955837bd0448fcd51a9
Ehimmell <himmellethan@gmail.com>
2023-07-24 18:51:20 -0500

    add vision testing classes

---
+++ 2023OffSeason/src/main/java/java/robot/camera.java
@@ -0,0 +1,9 @@
+package java.robot;
+
+import org.photonvision.PhotonCamera;
+
+public class camera {
+    PhotonCamera testCamera = new PhotonCamera("USB_Camera-B4.09.24.1");
+
+    var result = camera.getLatestResult();
+}
---
+++ 2023OffSeason/src/main/java/java/robot/cameraTesting.java
@@ -0,0 +1,5 @@
+package java.robot;
+
+public class cameraTesting {
+    
+}
